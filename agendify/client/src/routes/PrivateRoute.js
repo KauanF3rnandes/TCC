b89d8274eeb36e383c01e768cb5bf9c7d5eedf8a
KauanF3rnandes <kauangabrielfp97@gmail.com>
2024-06-17 21:28:08 -0300

    update

--- agendify/client/src/routes/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { isAuthenticated } from './auth';
-
-const PrivateRoute = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
